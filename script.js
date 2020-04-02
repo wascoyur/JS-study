1fd6790cf0f4b34977788e22c1e2bab40260cc49
wascoyur <mytem20007@gmail.com>
2020-04-02 17:29:01 +0500

    Задание 06-hard выполнено

--- script.js
+++ script.js
@@ -16,21 +16,43 @@ function cl(message /* , hint='' */) {
 		}
 		let result = secret - getAnswerUser;
 		count++;
+		attempt--;
+		if(attempt <= 0){
+			let a = confirm('Попытки закончились, хотите сыграть еще?');
+			if(a){
+				reset();
+				func();
+			}else{
+				return;
+			}
+		}
 
 		if (result > 0) {
-			console.log('Загаданное число больше');
+			console.log(`Загаданное число больше, количество оставшихся попыток: ${attempt}`);
 			exec();
 		} else if (result < 0) {
-			console.log('Загаданное число меньше');
+			console.log(`Загаданное число меньше, количество оставшихся попыток: ${attempt}`);
 			exec();
 		} else {
-			console.log('Бинго!!! Вы угадали!!!');
-			console.log(`Количество попыток: ${count}`);
+			console.log(`Поздравляю, Вы угадали!!! Количество попыток: ${count}`);
+			let conf = confirm('Хотели бы сыграть еще?');
+			if (conf) {
+				count = 0;
+				attempt = 10;
+				func();
+			}else{
+				console.log('Пока!');
+			}
 			return;
 		}
 	};
+	function reset() {
+		count = 0;
+		attempt = 10;
+	}
 	return exec;
 }
+
 
 let func = cl('Угадай число от 1 до 100');
 func();
