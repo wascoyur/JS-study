269fbb6ae25089443ebe6098ae33309acaa1f2f5
wascoyur <mytem20007@gmail.com>
2020-04-02 15:55:26 +0500

    задание 06 выполнено

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
 // let answerUser = '';
 //========functions========
-function cl(message/* , hint='' */){
-	let secret = parseInt(Math.random()*100);
+function cl(message /* , hint='' */) {
+	let secret = parseInt(Math.random() * 100);
 	let count = 0;
-	let myFunc = function(){
-		let getAnswerUser = parseInt(prompt(message).trim());
+	let attempt = 10;
+	let exec = function() {
+		let getAnswerUser = (prompt(message));
+		if(getAnswerUser === null){
+			return;
+		}
+		let t = Number.isInteger(parseInt(getAnswerUser));
+		if(!t){
+			console.log('Введи число!');
+			exec();
+		}
 		let result = secret - getAnswerUser;
 		count++;
 
 		if (result > 0) {
 			console.log('Загаданное число больше');
-		}else if(result < 0){
+			exec();
+		} else if (result < 0) {
 			console.log('Загаданное число меньше');
-		}else{
+			exec();
+		} else {
 			console.log('Бинго!!! Вы угадали!!!');
+			console.log(`Количество попыток: ${count}`);
+			return;
 		}
 	};
-	return myFunc;
+	return exec;
 }
-cl('me');
-console.log(cl('t'));
+
+let func = cl('Угадай число от 1 до 100');
+func();
