c8c2dad45175dde96bd0c37dad7cc26b813f01d9
wascoyur <mytem20007@gmail.com>
2020-04-07 21:50:35 +0500

    не работает, убрал консоль логи

--- test.js
+++ test.js
@@ -1,33 +1,14 @@
 let buttonStart = document.querySelector("#start");
-console.dir(buttonStart);
 let buttonIncomPlus = document.querySelectorAll('button')[0];
-console.log(buttonIncomPlus);
 let buttonExpensAdd = document.querySelectorAll('button')[1];
-
-console.log(buttonExpensAdd);
 let depositCheck = document.querySelector("#deposit-check");
-console.log(depositCheck);
 let additionalIncomeItem0 = document.querySelectorAll('.additional_income-item')[0];
 let additionalIncomeItem1 = document.querySelectorAll('.additional_income-item')[1];
-console.log(additionalIncomeItem0);
-console.log(additionalIncomeItem1);
 let resultTotalBudgetMonthValue = document.querySelector('.result-total budget_month-value');
-let resultTotalBudgetDayValue = document.querySelector(
-  ".result-total budget_day-value"
-);
-let resultTotaExpensesMonthValue = document.querySelector(
-  ".result-total expenses_month-value"
-);
-let resultTotalAdditionalIncomeValue = document.querySelector(
-  ".result-total additional_income-value"
-);
-let resultTotalAdditionalExpensesValue = document.querySelector(
-  ".result-total additional_expenses-value"
-);
-let resultTotalIncomePeriodValue = document.querySelector(
-  ".result-total income_period-value"
-);
-let resultTotalTargetMonthValue = document.querySelector(
-  ".result-total target_month-value"
-);
-console.log(value);
+let resultTotalBudgetDayValue = document.querySelector(".result-total budget_day-value");
+let resultTotaExpensesMonthValue = document.querySelector(".result-total expenses_month-value");
+let resultTotalAdditionalIncomeValue = document.querySelector(".result-total additional_income-value");
+let resultTotalAdditionalExpensesValue = document.querySelector(".result-total additional_expenses-value");
+let resultTotalIncomePeriodValue = document.querySelector(".result-total income_period-value");
+let resultTotalTargetMonthValue = document.querySelector(".result-total target_month-value");
+let periodSelect = document.querySelector('.period-select')
