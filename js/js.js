122d5558fb6d682df75a46f6a6014b8b6498c9ab
wascoyur <mytem20007@gmail.com>
2020-04-17 14:55:36 +0500

    в рендер добавлен ли

---
+++ js/js.js
@@ -0,0 +1,9 @@
+'use strict';
+let todoControl = document.querySelector('.todo-control') /* форма */;
+let headerInput = document.querySelector('.header-input') ;/* значения  */
+let todoList = document.querySelector('.todo-list');/* дела  */
+let todoCompleted = document.querySelector('.todo-completed');/* выполненные дела  */
+const todoData = [
+    {},
+    {}
+]
