74e0da6307f785550ea735e6013cb51af2da6ebc
wascoyur <mytem20007@gmail.com>
2020-04-17 15:02:56 +0500

    добавлена верстка

--- js/js.js
+++ js/js.js
@@ -4,6 +4,31 @@ let headerInput = document.querySelector('.header-input') ;/* значения  
 let todoList = document.querySelector('.todo-list');/* дела  */
 let todoCompleted = document.querySelector('.todo-completed');/* выполненные дела  */
 const todoData = [
-    {},
-    {}
-]
+    {
+        value:'сварить кофе',
+        completed: false
+    },
+    {
+        value:'помыть посуду',
+        completed: false
+    }
+];
+
+const render = function(){
+    todoData.forEach(function(item){
+        const li = document.createElement('li');
+        li.classList.add('todo-item');
+        li.innerHTML = '<span class="text-todo">'+ item.value + '</span>';
+    })
+};
+
+todoControl.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    const newTodo = {
+      value: "сварить кофе",
+      completed: false,
+    };
+    todoData.push(newTodo);
+} )
+render();
