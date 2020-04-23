10f9486296e89355bb347e150a1cf5b88a393cd7
yur <mytem20007@gmail.com>
2020-04-23 17:12:24 +0500

    таймер по видео сделан

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
-setTimeout(function(){
-  console.log('Msg');
-}, 3000)
+window.addEventListener('DOMContentLoaded',function () {
+  'use strict';
+  function countTimer(deadline) {
+    let timerHours = document.querySelector("#timer-hours"),
+          timerMinutes = document.querySelector("#timer-minutes"),
+          timerSeconds = document.querySelector("#timer-seconds");
 
-let count = 0;
-setTimeout(function(){
-  console.log('Hellow ' + count);
-}, 2200)
-clearInterval();
- 
+    function getTimeRemaning() {
+      let dateNow = new Date().getTime(),
+        dateStop = new Date(deadline).getTime(),
+        timeRemining = (dateStop - dateNow)/1000,
+        seconds = Math.floor(timeRemining%60),
+        minutes = Math.floor((timeRemining/60)%60),
+        hourse = Math.floor((timeRemining/60/60)%24),
+        day = Math.floor((timeRemining/60/60)/24);
+      return{timeRemining, hourse, minutes, seconds};
+    };
+
+    function updateClock(params) {
+      let timer = getTimeRemaning();
+      timerHours.textContent = timer.hourse;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+
+      if (timer.timeRemining > 0) {
+        setTimeout(updateClock, 1000)
+      }
+    }
+    updateClock();
+
+  }
+  // countTimer('01 july 2020');
+  setInterval(countTimer("01 july 2020"), 1000);
+})
