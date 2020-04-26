05df873048d0746ef80448498e5a456130da14ce
wascoyur <mytem20007@gmail.com>
2020-04-26 13:55:23 +0500

    не срабатывает останов по наведению на стрелки

--- index.js
+++ index.js
@@ -120,6 +120,7 @@ window.addEventListener("DOMContentLoaded", function () {
     const dot = document.querySelectorAll('.dot');
     const slider = document.querySelector('.portfolio-content');
     let currentSlide = 0;
+    let interval;
 
     const nextSlide = (elem, index, strClass)=>{
       elem[index].classList.add(strClass);
@@ -139,11 +140,52 @@ window.addEventListener("DOMContentLoaded", function () {
       nextSlide(dot, currentSlide, "dot-active");
     }
     const startSlide = (time)=>{
-      setInterval(autoPlaySlide, time);
+      interval = setInterval(autoPlaySlide, time);
     }
     const stopSlide = ()=>{
-
+      clearInterval(interval);
     }
+    slider.addEventListener('click', (event) =>{
+      event.preventDefault();
+      let target = event.target;
+
+      if (!target.matches(".portfolio-btn, .dot"))return;
+      prevSlide(slide, currentSlide, "portfolio-item-active");
+      prevSlide(dot, currentSlide, "dot-active");
+
+      if (target.matches(".next")) {
+        currentSlide++;
+        if (currentSlide >= slide.length) {
+          currentSlide = 0;
+        }
+      } else if (target.matches(".prev")) {
+        currentSlide--;
+        if (currentSlide < 0) {
+          currentSlide = slide.length -1;
+        }
+      } else if(target.matches('.dot')){
+          dot.forEach((elem, index) =>{
+            if (elem === target) {
+                currentSlide = index;
+            }
+          })
+      }
+      nextSlide(slide, currentSlide, "portfolio-item-active");
+      nextSlide(dot, currentSlide, "dot-active");
+    })
+
+    slider.addEventListener('mouseenter', (event) =>{
+      if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')){
+        stopSlide();
+        console.log('stopSlide: ');
+      }
+    });
+    slider.addEventListener('mouseleave', (event) =>{
+      if(event.target.matches('.portfolio-btn') || event.target.matches('.dot')){
+        startSlide();
+        console.log('startSlide: ');
+      }
+    });
     startSlide(1500);
   }
   slider();
