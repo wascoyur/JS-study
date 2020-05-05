bb7939b50e79c98b7fbaddf1fe3cd533235788cb
wascoyur <mytem20007@gmail.com>
2020-05-05 12:28:46 +0500

    добавлена фильтрация полей имени

--- index.js
+++ index.js
@@ -364,14 +364,23 @@ window.addEventListener('DOMContentLoaded', () => {
   };
   sendForm();
   const phones = document.querySelectorAll("input[type='tel']");
+  const userNames = document.querySelectorAll("input[name='user_name']");
   const validationTel = (listPhones) => {
     listPhones.forEach((fieldTel) => {
       fieldTel.addEventListener('input', () => {
         console.log(fieldTel.value);
-        fieldTel.value = fieldTel.value.match(/^[0-9 ()+-]+$/g/* , '' */);
-        console.log("fieldTel.value.(/^[0-9]/g, ''): ", fieldTel.value.replace(/^[0-9]/g, ''));
+        fieldTel.value = fieldTel.value.match(/^[0-9 ()+-]+$/g);
+      });
+    });
+  };
+  const validationUserName = (listUserNames) => {
+    listUserNames.forEach((fieldUname) => {
+      fieldUname.addEventListener('input', () => {
+        console.log(fieldUname.value);
+        fieldUname.value = fieldUname.value.match(/[А-я]+$/g);
       });
     });
   };
   validationTel(phones);
+  validationUserName(userNames);
 });
