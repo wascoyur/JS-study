8529456eadbafeb0e851c663752ab9a27f609a3f
wascoyur <mytem20007@gmail.com>
2020-05-06 19:44:59 +0500

    12:00

--- promise.js
+++ promise.js
@@ -1,22 +1,32 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable no-console */
 const output = document.getElementById('output');
-const getData = (url, outputData) => {
-  const request = new XMLHttpRequest();
-  request.open('GET', url);
-  request.addEventListener('readystatechange', () => {
-    if (request.readyState !== 4) {
-      return;
-    }
-    if (request.status === 200) {
-      const response = JSON.parse(request.responseText);
-      outputData(response);
-    } else {
-      console.error(request.statusText);
-    }
+const getData = (url) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('readystatechange', () => {
+      if (request.readyState !== 4) {
+        return;
+      }
+      if (request.status === 200) {
+        const response = JSON.parse(request.responseText);
+        resolve(response);
+      } else {
+        reject(request.statusText);
+      }
+    });
+    request.send();
   });
-  request.send();
+
 };
-getData('https://jsonplaceholder.typicode.com/photos', (data) => {
-  console.log(data);
-});
+const outputPhotos = (data) => {
+  const random = Math.floor(Math.random() * data.length);
+  const obj = data[random];
+  output.innerHTML = `<h4>${obj.title}</h4>
+                      <img src = "${obj.thumbnailUrl}" alt="${obj.title}">`;
+};
+const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
+getData(urlPhotos)
+  .then(outputPhotos)
+  .catch((error) => console.error(error));
