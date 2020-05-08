12ffbf96cdfbe83d9f42390d43f2e181903eba91
wascoyur <mytem20007@gmail.com>
2020-05-08 13:16:28 +0500

    start

--- promise.js
+++ promise.js
@@ -1,38 +1,19 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-console */
-const output = document.getElementById('output');
-const getData = (url) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState !== 4) {
-        return;
-      }
-      if (request.status === 200) {
-        const response = JSON.parse(request.responseText);
-        resolve(response);
-      } else {
-        reject(request.statusText);
-      }
-    });
-    request.send();
-  });
-
+const str = {
+  "name": "Ancient One",
+  "species": "human",
+  "gender": "Female",
+  "birthDay": "1316",
+  "deathDay": "2017",
+  "status": "deceased",
+  "actors": "Tilda Swinton",
+  "photo": "dbimage/DS_Ancient_One_Poster_cropped.png",
+  "movies": [
+    "Doctor Strange",
+    "Avengers: Endgame"
+  ]
 };
-const outputPhotos = (data) => {
-  // const random = Math.floor(Math.random() * data.length);
-  // const obj = data[random];
-  output.insertAdjacentHTML('beforebegin',
-    `<h4>${data.title}</h4>
-      <img src="${data.thumbnailUrl}" alt="${data.title}">`);
-};
-const urlPhotos = 'https://jsonplaceholder.typicode.com/photos';
-const oneImg = getData('https://jsonplaceholder.typicode.com/photos/1');
-const twoImg = getData('https://jsonplaceholder.typicode.com/photos/2');
-oneImg
-  .then(outputPhotos)
-  .catch((error) => console.log(error));
-twoImg
-  .then(outputPhotos)
-  .catch((error) => console.log(error));
+let оb = JSON.stringify(str);
+console.log(ob);
+const jsonFile = JSON.parse("./dbHeroes/dbHeroes.json");
+const arrCards = (jsonFile);
+// arrCards.
