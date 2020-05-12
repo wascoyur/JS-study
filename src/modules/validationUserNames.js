b9aa4b7a443b6fbe16d5aa144a05e5003ec6633d
wascoyur <mytem20007@gmail.com>
2020-05-12 19:28:41 +0500

    Валидейшн юзер нейм не видит е

--- src/modules/validationUserNames.js
+++ src/modules/validationUserNames.js
@@ -3,8 +3,18 @@
 const validationUserName = (listUserNames) => {
   listUserNames.forEach((fieldUname) => {
     fieldUname.addEventListener('input', () => {
-      console.log(fieldUname.value);
-      fieldUname.value = fieldUname.value.match(/[А-я, A-z]+$/g);
+      if (fieldUname.value) {
+        // let arrFieldName = (fieldUname.value).split('');
+        const inp = fieldUname.value;
+        const oldVal = inp.slice(0, (inp.length - 1));
+        // const insert = arrFieldName[arrFieldName.length - 1]/* .match(/[А-я, A-z]+$/g) */;
+        const insert = inp[fieldUname.value.length - 1];
+        fieldUname.value = oldVal + insert;
+      } else {
+        return;
+      }
+
+
     });
   });
 };
