c772afde27d56946bf5b9964e0a1a34e0cb01996
wascoyur <mytem20007@gmail.com>
2020-05-12 20:34:31 +0500

    формы для телефонов принимают только цифры и +

--- src/modules/validationTel.js
+++ src/modules/validationTel.js
@@ -4,7 +4,13 @@ const validationTel = (listPhones) => {
   listPhones.forEach((fieldTel) => {
     fieldTel.addEventListener('input', () => {
       console.log(fieldTel.value);
-      fieldTel.value = fieldTel.value.match(/^[0-9 ()+-]+$/g);
+      // fieldTel.value = fieldTel.value.match(/^[0-9 ()+-]+$/g);
+      if (fieldTel.value) {
+        const inp = fieldTel.value;
+        const oldVal = inp.slice(0, (inp.length - 1));
+        const insert = inp[fieldTel.value.length - 1].replace(/[^((8|+7)), ^(0-9)]+$/g, '');
+        fieldTel.value = oldVal + insert;
+      }
     });
   });
 };
--- src/modules/validationUserNames.js
+++ src/modules/validationUserNames.js
@@ -2,17 +2,13 @@
 
 const validationUserName = (listUserNames) => {
   listUserNames.forEach((fieldUname) => {
-    fieldUname.addEventListener('input', (event) => {
+    fieldUname.addEventListener('input', () => {
       if (fieldUname.value) {
         const inp = fieldUname.value;
         const oldVal = inp.slice(0, (inp.length - 1));
         const insert = inp[fieldUname.value.length - 1].replace(/[^(А-я), ^(A-z)]+$/g, '');
         fieldUname.value = oldVal + insert;
-      } else {
-        return;
       }
-
-
     });
   });
 };
