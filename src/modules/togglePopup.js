bacd54ade65d8a3c7a21eb3d835389d9d5f20ecc
wascoyur <mytem20007@gmail.com>
2020-05-13 21:11:33 +0500

    плавность модальное окно готово

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,21 +1,43 @@
 const togglePopUp = () => {
   const popup = document.querySelector('.popup');
-  const btnPopup = document.querySelectorAll('.popup-btn');
-  const closePopup = document.querySelector('.popup-close');
-  btnPopup.forEach((el) => {
+  const popupContent = document.querySelector('.popup-content');
+  const popupBtn = document.querySelectorAll('.popup-btn');
+
+  let count = -200;
+  const animate = () => {
+    if (document.documentElement.clientWidth < 768) {
+      popupContent.style.transform = `translate(0)`;
+      return;
+    }
+    let requestId = requestAnimationFrame(animate);
+    count += 5;
+    popupContent.style.transform = `translate(${count}%)`;
+    if (count >= 0) {
+      cancelAnimationFrame(requestId);
+          
+    }
+  };
+
+  popupBtn.forEach(el => {
     el.addEventListener('click', () => {
       popup.style.display = 'block';
+      animate();
     });
   });
-  closePopup.addEventListener('click', () => {
-    popup.style.display = 'none';
-  });
+
   popup.addEventListener('click', (event) => {
-    let { target } = event;
-    target = target.closest('.popup-content');
-    if (!target) {
+    let target = event.target;
+    count = -200; 
+    if (target.classList.contains('popup-close')) {
       popup.style.display = 'none';
+    } else {
+      target = target.closest('.popup-content');
+      if (!target) {
+        popup.style.display = 'none';
+      }
     }
   });
+
+
 };
 export default togglePopUp;
