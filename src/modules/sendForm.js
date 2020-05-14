076484ff4e1f8dbfca1264eb8b9525ddb14ef132
wascoyur <mytem20007@gmail.com>
2020-05-14 19:36:49 +0500

    добавил обработку form2

--- src/modules/sendForm.js
+++ src/modules/sendForm.js
@@ -6,6 +6,7 @@ const sendForm = () => {
   const successMsg = 'Спасибо, мы скоро с Вами свяжемся';
 
   const form = document.getElementById('form1');
+  const form2 = document.getElementById('form2');
   const form3 = document.getElementById('form3');
   const statusMsg = document.createElement('div');
   statusMsg.classList.add('status-message');
@@ -23,29 +24,25 @@ const sendForm = () => {
     event.preventDefault();
     form.appendChild(statusMsg);
     const formData = new FormData(form);
-    // formData.status
-    let body = {};
+    const body = {};
     for (const val of formData.entries()) {
       body[val[0]] = val[1];
     }
-    console.log('formData.status: ', formData);
-    const postData = (data) => {
-      return fetch('./server.php', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-        mode: 'cors',
-      });
-    };
+    const postData = (data) => fetch('./server.php', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+      mode: 'cors',
+    });
     postData(body)
       .then(statusMsg.textContent = successMsg)
       .catch((error) => {
         statusMsg.textContent = errorMsg;
         console.error(error);
       });
-    // form.reset();
+    form.reset();
   });
 
   form3.addEventListener('submit', (event) => {
@@ -54,8 +51,50 @@ const sendForm = () => {
     statusMsg.style.cssText = `font-size: 2rem;
             color: #fff; `;
     const formData = new FormData(form3);
-    let body = {};
-    for (let val of formData.entries()) {
+    const body = {};
+    for (const val of formData.entries()) {
+      body[val[0]] = val[1];
+    }
+    const postData = (req) => {
+      return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
+        request.addEventListener('readystatechange', () => {
+          statusMsg.textContent = loadMsg;
+          if (request.readyState !== 4) {
+            return;
+          }
+          if (request.status === 200) {
+            statusMsg.textContent = successMsg;
+            resolve();
+          } else {
+            statusMsg.textContent = errorMsg;
+            reject();
+          }
+        });
+        request.open('POST', './server.php');
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(JSON.stringify(req));
+        removeStatusMessage();
+      });
+    };
+    postData(body)
+      .then(statusMsg.textContent = successMsg)
+      .catch((error) => {
+        statusMsg.textContent = errorMsg;
+        console.error(error);
+      });
+    form3.reset();
+    removeStatusMessage();
+  });
+  
+  form2.addEventListener('submit', (event) => {
+    event.preventDefault();
+    form3.appendChild(statusMsg);
+    statusMsg.style.cssText = `font-size: 2rem;
+            color: #fff; `;
+    const formData = new FormData(form2);
+    const body = {};
+    for (const val of formData.entries()) {
       body[val[0]] = val[1];
     }
     const postData = (req) => {
